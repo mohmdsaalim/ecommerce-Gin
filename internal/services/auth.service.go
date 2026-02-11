da616c70b3db4866bd013fbe05d976aa62a52a8d
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-02-11 23:19:03 +0530

    fixed the otp bug

--- internal/services/auth.service.go
+++ internal/services/auth.service.go
@@ -2,21 +2,22 @@ package services
 
 import (
 	"errors"
-	// "fmt"
-	// "time"
+	"fmt"
+	"time"
 
-	// "github.com/mohmdsaalim/ecommerce-Gin/cmd/worker"
 	"github.com/mohmdsaalim/ecommerce-Gin/internal/models"
 	"github.com/mohmdsaalim/ecommerce-Gin/internal/repositories"
+	"github.com/mohmdsaalim/ecommerce-Gin/internal/services/workers"
 	"github.com/mohmdsaalim/ecommerce-Gin/internal/utils"
-	// "github.com/mohmdsaalim/ecommerce-Gin/pkg/database"
+	"github.com/mohmdsaalim/ecommerce-Gin/pkg/database"
 	"github.com/redis/go-redis/v9"
 )
 
 type AuthService struct {
-	repo repositories.Repository
+	repo  repositories.Repository
 	redis *redis.Client
 }
+
 // costructer from main -> service
 func NewAuthService(repo repositories.Repository, redisClient *redis.Client) *AuthService {
 	return &AuthService{repo: repo, redis: redisClient}
@@ -26,7 +27,7 @@ func NewAuthService(repo repositories.Repository, redisClient *redis.Client) *Au
 func (s *AuthService) Register(username, email, password string) error {
 	// need to check the user alredy exist...
 	hashedPassword, err := utils.HashPassword(password)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
@@ -39,26 +40,37 @@ func (s *AuthService) Register(username, email, password string) error {
 		EmailVerified: false,
 	}
 
-	return  s.repo.Insert(user)
+	err = s.repo.Insert(user)
+	if err != nil {
+		return err
+	}
 
-	// otp, _:= utils.GenerateOTP(1)
-	// key := fmt.Sprintf("otp:email_verification:%d", user.ID)
+	// Generate 6 digit OTP
+	otp, _ := utils.GenerateOTP(6)
+	key := fmt.Sprintf("otp:%s", user.Email)
 
-	// err = s.redis.Set(database.Ctx, key, otp, 5*time.Minute).Err()
-	// if err != nil{
-	// 	return err
-	// }
-	// // worker.OTPChanne
-	// return nil
+	// Store OTP in redis for 5 minutes
+	err = database.SetOTP(key, otp, 5*time.Minute)
+	if err != nil {
+		return err
+	}
+
+	// Send to worker channel
+	workers.OTPChannel <- workers.OTPJob{
+		Email: user.Email,
+		Code:  otp,
+	}
+
+	return nil
 }
 
-// validation 
+// validation
 
 func (s *AuthService) Login(email, password string) (string, error) {
 	var user models.User
 
 	//  Fetch user by email
-	if err := s.repo.FindOne(&user, "email = ?",nil, email,); err != nil {
+	if err := s.repo.FindOne(&user, "email = ?", nil, email); err != nil {
 		return "", errors.New("invalid Email")
 	}
 
@@ -75,3 +87,94 @@ func (s *AuthService) Login(email, password string) (string, error) {
 
 	return token, nil
 }
+
+
+// package services
+
+// import (
+// 	"errors"
+// 	"fmt"
+// 	"time"
+
+// 	"github.com/mohmdsaalim/ecommerce-Gin/internal/models"
+// 	"github.com/mohmdsaalim/ecommerce-Gin/internal/repositories"
+// 	"github.com/mohmdsaalim/ecommerce-Gin/internal/services/workers"
+// 	"github.com/mohmdsaalim/ecommerce-Gin/internal/utils"
+// 	"github.com/mohmdsaalim/ecommerce-Gin/pkg/database"
+// 	"github.com/redis/go-redis/v9"
+// )
+
+// type AuthService struct {
+// 	repo  repositories.Repository
+// 	redis *redis.Client
+// }
+
+// // costructer from main -> service
+// func NewAuthService(repo repositories.Repository, redisClient *redis.Client) *AuthService {
+// 	return &AuthService{repo: repo, redis: redisClient}
+// }
+
+// // register service
+// func (s *AuthService) Register(username, email, password string) error {
+// 	// need to check the user alredy exist...
+// 	hashedPassword, err := utils.HashPassword(password)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	user := &models.User{
+// 		Username:      username,
+// 		Email:         email,
+// 		PasswordHash:  hashedPassword,
+// 		Role:          "USER",
+// 		Status:        "active",
+// 		EmailVerified: false,
+// 	}
+
+// 	err = s.repo.Insert(user)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Generate 6 digit OTP
+// 	otp, _ := utils.GenerateOTP(6)
+// 	key := fmt.Sprintf("otp:%s", user.Email)
+
+// 	// Store OTP in redis for 5 minutes
+// 	err = database.SetOTP(key, otp, 5*time.Minute)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Send to worker channel
+// 	workers.OTPChannel <- workers.OTPJob{
+// 		Email: user.Email,
+// 		Code:  otp,
+// 	}
+
+// 	return nil
+// }
+
+// // validation
+
+// func (s *AuthService) Login(email, password string) (string, error) {
+// 	var user models.User
+
+// 	//  Fetch user by email
+// 	if err := s.repo.FindOne(&user, "email = ?", nil, email); err != nil {
+// 		return "", errors.New("invalid Email")
+// 	}
+
+// 	// Check password
+// 	if !utils.Checkpassword(user.PasswordHash, password) {
+// 		return "", errors.New("invalid Password")
+// 	}
+
+// 	//  Generate JWT
+// 	token, err := utils.GenerateToken(user.ID, user.Role)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return token, nil
+// }
